9144e4fd962e44fad40cfabc379f166f7babd24e
Abhinav Siva Ajith <abhiajith42059@gmail.com>
2025-10-18 03:03:29 +0530

    gpt5testing

--- app.py
+++ app.py
@@ -431,8 +431,7 @@ content
                     "content": prompt
                 }
             ],
-            temperature=0.7,
-            max_tokens=4000
+            
         )
         
         generated_text = response.choices[0].message.content
